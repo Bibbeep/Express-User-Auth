c88328a838664c0da523edb035719955ae225539
Bibbeep <muhammadhabibalfarabi290803@gmail.com>
2025-02-06 04:30:21 +0700

    chore: moved documentation routes and health routes directly inside index route file

--- src/routes/health.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { verifyToken } = require('../middlewares/authentication');
-const { adminAccess } = require('../middlewares/authorization');
-const { getAPIHealth } = require('../controllers/health.controller');
-
-router.get('/health', verifyToken, adminAccess, getAPIHealth);
-
-module.exports = router;
