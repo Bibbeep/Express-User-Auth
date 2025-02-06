71324a9c6ec112fbe51dd201655e0b8fcd8a72b0
Bibbeep <muhammadhabibalfarabi290803@gmail.com>
2025-02-06 23:57:34 +0700

    feat(health-check): added health check endpoint

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -1,7 +1,24 @@
+const Health = require('../models/health');
+
 module.exports = {
     getAPIHealth: async (req, res, next) => {
         try {
-            /* Empty */
+            const data = {
+                application: Health.getApplicationHealth(),
+                database: await Health.getDatabaseHealth(),
+                cache: await Health.getCacheHealth(),
+                system: Health.getSystemHealth(),
+                timestamp: new Date(Date.now()),
+            };
+
+            return res.status(200).json({
+                success: true,
+                statusCode: 200,
+                message:
+                    'Successfully retrieved endpoints health check status.',
+                data,
+                errors: null,
+            });
         } catch (err) {
             next(err);
         }
