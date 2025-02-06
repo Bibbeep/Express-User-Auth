a3566f7c08d6418e908544bb541be0fd4f7ed3ec
Bibbeep <muhammadhabibalfarabi290803@gmail.com>
2025-02-06 23:06:04 +0700

    feat(health-check): implemented admin access authorization middleware

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,7 +1,13 @@
+const HTTPError = require('../utils/httpError');
+
 module.exports = {
     adminAccess: async (req, res, next) => {
         try {
-            /* Empty */
+            if (req.userRole !== 'ADMIN') {
+                throw new HTTPError(403, 'Restricted access.');
+            }
+
+            next();
         } catch (err) {
             next(err);
         }
